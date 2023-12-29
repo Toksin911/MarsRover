e4bee1e19c87db727a059ee3c94a42c0355e402b
Daniil <serbjasha2@yandex.ru>
2023-12-29 21:58:15 +0300

    Fixed

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# 104042728
+# 104047724
 def mars_rover(robots: list[int], limit: int) -> int:
     """Рассчитать количество платформ."""
     platforms: int = 0  # Число платформ.
     position_max: int = len(robots) - 1  # Наибольшое число в моссиве.
     position_min: int = 0  # Наименьшое число в моссиве.
+    robot: list[int] = sorted(robots)
     while position_max >= position_min:
-        if (robots[position_max] + robots[position_min]) <= limit:
+        if (robot[position_max] + robot[position_min]) <= limit:
             position_min += 1
         position_max -= 1
         platforms += 1
@@ -15,5 +16,4 @@ def mars_rover(robots: list[int], limit: int) -> int:
 if __name__ == '__main__':
     robots: list[int] = [int(array_count) for array_count in input().split()]
     limit = int(input())
-    robots.sort()
     print(mars_rover(robots, limit))
