174d5951631a293406f16d130da5c10be13bb13a
Daniil <serbjasha2@yandex.ru>
2023-12-29 10:47:36 +0300

    Fixed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ def mars_rover(robots: list[int], limit: int) -> int:
     platforms: int = 0  # Число платформ.
     position_max: int = len(robots) - 1  # Наибольшое число в моссиве.
     position_min: int = 0  # Наименьшое число в моссиве.
-    robots.sort()
     while position_max >= position_min:
         if (robots[position_max] + robots[position_min]) <= limit:
             position_min += 1
@@ -16,5 +15,5 @@ def mars_rover(robots: list[int], limit: int) -> int:
 if __name__ == '__main__':
     robots: list[int] = [int(array_count) for array_count in input().split()]
     limit = int(input())
+    robots.sort()
     print(mars_rover(robots, limit))
- 
